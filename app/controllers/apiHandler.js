dfa1082ee124349e7ddebde94b4bfeba879cc290
Glitch (juvenile-birch) <none>
2019-02-09 12:38:55 +0000

    📇🏊🏼 Checkpoint ./app/controllers/apiHandler.js:553280/276

--- app/controllers/apiHandler.js
+++ app/controllers/apiHandler.js
@@ -2,20 +2,27 @@
 
 const apiKey = process.env.API_KEY;
 const ajax = require('../common/ajax-functions.js');
-const https = require('https');
+const https = require('https'),
+      fs = require('fs'),
+      stream = require('stream');
 
 function apiHandler() {
   this.getStocks = (req, res) => {
     console.log(req.query)
     let url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=demo'
     
-    https.get(url, function(res) {
+//     https.get(url, function(res) {
       
-      res.on('data', (d) => {
-        console.log(d)
-      });
+//       res.on('data', (d) => {
+//         console.log(fs.createReadStream(d))
+//       });
       
-    });
+//     });
+    https.get(url).then(res => {
+      
+    }).catch(err => {
+  
+    })
     
   }
 
