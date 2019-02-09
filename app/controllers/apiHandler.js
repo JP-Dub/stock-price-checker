07990538169c91dc947d7307689cc56e4b76baed
Glitch (juvenile-birch) <none>
2019-02-09 12:49:42 +0000

    💰📒 Checkpoint ./app/controllers/apiHandler.js:553280/64

--- app/controllers/apiHandler.js
+++ app/controllers/apiHandler.js
@@ -18,10 +18,10 @@ function apiHandler() {
 //       });
       
 //     });
-    https.get(url).then(res => {
-      
+    https.get(url).on('data', (res) => {
+      console.log(res)
     }).catch(err => {
-  
+      console.log(err)
     })
     
   }
