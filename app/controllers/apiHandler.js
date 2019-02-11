2f9435be97c564eee794b89367b62cb4c0ee87ea
Glitch (juvenile-birch) <none>
2019-02-11 00:06:16 +0000

    🚵🏉 Checkpoint ./app/controllers/apiHandler.js:553280/1688

--- app/controllers/apiHandler.js
+++ app/controllers/apiHandler.js
@@ -7,45 +7,57 @@ function apiHandler() {
   
   this.getStocks = (req, res) => {
     console.log(req.query)
-    let symbol = req.query.stock,
-        url    = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=' + symbol + '&apikey=' + apiKey;
+    let symbol = req.query.stock;
+        
     
     //let url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=' + apiKey
-  const checkStock   
-    https.get(url, (res) => {
-      const { statusCode } = res;
-      const contentType = res.headers['content-type'];
+    const stockPrices = (symbol, done) =>{   
+      let url    = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=' + symbol + '&apikey=' + apiKey;
+      https.get(url, (res) => {
+        const { statusCode } = res;
+        const contentType = res.headers['content-type'];
 
-      let error;
-      if (statusCode !== 200) {
-        error = new Error('Request Failed.\n' +
-                          `Status Code: ${statusCode}`);
-      } else if (!/^application\/json/.test(contentType)) {
-        error = new Error('Invalid content-type.\n' +
-                          `Expected application/json but received ${contentType}`);
-      }
-      if (error) {
-        console.error(error.message);
-        // consume response data to free up memory
-        res.resume();
-        return;
-      }
-
-      res.setEncoding('utf8');
-      let rawData = '';
-      res.on('data', (chunk) => { rawData += chunk; });
-      res.on('end', () => {
-        try {
-          const parsedData = JSON.parse(rawData);
-          console.log(parsedData);
-        } catch (e) {
-          console.error(e.message);
+        let error;
+        if (statusCode !== 200) {
+          error = new Error('Request Failed.\n' +
+                            `Status Code: ${statusCode}`);
+        } else if (!/^application\/json/.test(contentType)) {
+          error = new Error('Invalid content-type.\n' +
+                            `Expected application/json but received ${contentType}`);
         }
-      });
-    }).on('error', (e) => {
-      console.error(`Got error: ${e.message}`);
-    }); // end of https request
+        if (error) {
+          console.error(error.message);
+          // consume response data to free up memory
+          res.resume();
+          return;
+        }
 
+        res.setEncoding('utf8');
+        let rawData = '';
+        res.on('data', (chunk) => { rawData += chunk; });
+        res.on('end', () => {
+          try {
+            const parsedData = JSON.parse(rawData);
+            console.log(parsedData);
+            done(parsedData);
+          } catch (e) {
+            console.error(e.message);
+          }
+        });
+      }).on('error', (e) => {
+        console.error(`Got error: ${e.message}`);
+      }); // end of https request
+    };
+    
+    Array.isArray(symbol) ? symbol.forEach( val => {
+                                        
+                              stockPrices(val, function done(stock) {
+                                console.log(stock)
+                              });
+                            })
+                            : stockPrices(symbol, function done(stock) {
+                                 console.log(stock)
+                              });
     
   };
 };
